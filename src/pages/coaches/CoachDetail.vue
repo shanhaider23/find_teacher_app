b0d63eb4cc9b3c40f35fd6bc3c9d9fd64d44856e
shanhaider23 <shan.haider@gmail.com>
2023-02-12 21:01:46 +0100

    base component

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,31 @@
 <template>
-    <section>Details</section>
+    <section><base-card>
+        <h2> {{ fullName }}</h2>
+        <h3> {{ rate }}/hour</h3>
+       </base-card>
+    </section>
+    
+    <section>
+        <base-card>
+    <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink"> Contact</base-button>
+    </header>
+    </base-card>
+    </section>
+    <section>
+    <base-card>
+    
+    
+    <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+    <p>{{ description }}</p>
+    </base-card>
+    </section>
     
     <router-link to="/coaches/c1/contact"></router-link>
 </template>
+<script>
+export default {
+    props:['id']
+}
+</script>
